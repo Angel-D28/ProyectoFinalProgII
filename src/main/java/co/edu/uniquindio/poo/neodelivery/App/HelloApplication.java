aca14f128f1992f160ccc102bb8cfbf23cb64048
Andres <carlosa.molinal@uqvirtual.edu.co>
2025-11-16 18:10:18 -0500

    Push antes de pullear cambios de la rama Diaz

--- src/main/java/co/edu/uniquindio/poo/neodelivery/App/HelloApplication.java
+++ src/main/java/co/edu/uniquindio/poo/neodelivery/App/HelloApplication.java
@@ -1,5 +1,7 @@
 package co.edu.uniquindio.poo.neodelivery.App;
 
+import co.edu.uniquindio.poo.neodelivery.model.gestores.ManageAdmin;
+import co.edu.uniquindio.poo.neodelivery.model.gestores.ManageReports;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
