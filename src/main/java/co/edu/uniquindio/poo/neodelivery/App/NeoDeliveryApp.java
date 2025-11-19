c9ba0c85c0ade72bc7d1598fb6808c00348909d1
Angel-D28 <angeld.diazp@uqvirtual.edu.co>
2025-11-18 23:19:44 -0500

    Se hace el commit despues de unir todas las ramas :felipeBranch, molinaBranch, diazBranch. el proyecto esta listo y se cambia el nombre de helloApplication por el correspondiente de la aplicacion

--- src/main/java/co/edu/uniquindio/poo/neodelivery/App/NeoDeliveryApp.java
+++ src/main/java/co/edu/uniquindio/poo/neodelivery/App/NeoDeliveryApp.java
@@ -1,11 +1,5 @@
 package co.edu.uniquindio.poo.neodelivery.App;
 
-import co.edu.uniquindio.poo.neodelivery.model.gestores.ManageAdmin;
-import co.edu.uniquindio.poo.neodelivery.model.gestores.ManageReports;
-import co.edu.uniquindio.poo.neodelivery.model.*;
-import co.edu.uniquindio.poo.neodelivery.model.Decorators.*;
-import co.edu.uniquindio.poo.neodelivery.model.gestores.ManagePayments;
-import co.edu.uniquindio.poo.neodelivery.model.gestores.ManageShipments;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -14,10 +8,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class NeoDeliveryApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/co/edu/uniquindio/poo/neodelivery/loginView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(NeoDeliveryApp.class.getResource("/co/edu/uniquindio/poo/neodelivery/loginView.fxml"));
         Image icon = new Image(getClass().getResourceAsStream("/images/logoCamionReportes.png"));
         stage.getIcons().add(icon);
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
